ed35f7f8abe1910794cc339ff4c13be05b945100
albanus n mutua <anm59@afsconnect2.njit.edu>
2019-11-01 00:41:39 -0400

    creation of registration form and new question form

--- is218project1.php
+++ is218project1.php
@@ -3,18 +3,40 @@
 $email = filter_input(INPUT_POST, 'email');
 $password = filter_input(INPUT_POST, 'password');
 
+$firstName = filter_input(INPUT_POST, 'firstName');
+$lastName = filter_input(INPUT_POST, 'lastName');
+$birthday = filter_input(INPUT_POST, 'birthday');
+$registration_email = filter_input(INPUT_POST, 'registration_email');
+$registration_password = filter_input(INPUT_POST, 'registration_password');
+
+$questionName = filter_input(INPUT_POST, 'questionname');
+$questionBody = filter_input(INPUT_POST, 'questionbody');
+$questionSkills = filter_input(INPUT_POST, 'questionSkills');
  ?>
+
 <! DOCTYPE html>
 <html>
 <head>
     <title>Display Email and Password</title>
 </head>
 <body>
-<h1>user information </h1>
-Email: <?php echo $email; ?><br><br>
+<h1>User Information </h1>
+Email: <?php echo $email; ?><br>
 Password: <?php echo $password;?><br>
 
-<h1>
+<h1>Registration  information </h1>
+First Name: <?php echo $firstName; ?><br>
+Last Name: <?php echo $lastName;?><br>
+Birthday: <?php echo $birthday;?><br>
+Email: <?php echo $registration_email; ?><br>
+Password: <?php echo $registration_password;?><br>
+
+
+<h1>Questions information </h1>
+Question Name: <?php echo $questionName; ?><br>
+Question Body: <?php echo $questionBody;?><br>
+Question Skills: <?php echo $questionSkills;?><br>
+
 
 </body>
 </html>
